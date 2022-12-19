341106603d9d65e626b71b8bc13e5982d4f4cbb9
Aaron Asch <aasch@athenahealth.com>
2022-12-19 14:49:13 -0500

    Dots are animated!

--- src/bpmn-modules/DotBpmnRendererModule.ts
+++ src/bpmn-modules/DotBpmnRendererModule.ts
@@ -1,10 +1,8 @@
 import { is } from 'bpmn-js/lib/util/ModelUtil';
 
-import { dataURL } from './nyan-cat';
 
-import { append as svgAppend, create as svgCreate, attr as svgAttr } from 'tiny-svg';
 import BpmnRenderer from 'bpmn-js/lib/draw/BpmnRenderer';
-import { getSemantic, getFillColor, getStrokeColor } from 'bpmn-js/lib/draw/BpmnRenderUtil';
+import '../css/animation-keyframes.css';
 
 class DotBpmnRenderer extends BpmnRenderer {
   constructor(
@@ -14,42 +12,14 @@ class DotBpmnRenderer extends BpmnRenderer {
     pathMap: any,
     canvas: any,
     textRenderer: any,
-    rendererPriority?: number
   ) {
     super(config, eventBus, styles, pathMap, canvas, textRenderer, 1500);
-    //TODO: https://usefulangle.com/post/39/adding-css-to-stylesheet-with-javascript#:~:text=Adding%20a%20New%20CSS%20Rule,rule%20to%20the%20stylesheet%20sheet.
   }
 
   static $inject: string[] = ['config', 'eventBus', 'styles', 'pathMap', 'canvas', 'textRenderer'];
 
   canRender(element: any) {
-    return is(element, 'bpmn:Task') || is(element, 'bpmn:SequenceFlow');
-  }
-
-  drawShape(parent: any, shape: any) {
-    const url = dataURL;
-
-    const catGfx = svgCreate('image', {
-      x: 0,
-      y: 0,
-      width: shape.width,
-      height: shape.height,
-      href: url,
-    }) as SVGElement;
-
-    svgAppend(parent, catGfx);
-
-    return catGfx;
-  }
-
-  private createPathFromConnection(connection: any) {
-    const waypoints = connection.waypoints;
-
-    let pathData = 'm  ' + waypoints[0].x + ',' + waypoints[0].y;
-    for (let i = 1; i < waypoints.length; i++) {
-      pathData += 'L' + waypoints[i].x + ',' + waypoints[i].y + ' ';
-    }
-    return pathData;
+    return is(element, 'bpmn:SequenceFlow');
   }
 
   drawConnection(visuals: any, connection: any) {
@@ -66,49 +36,6 @@ class DotBpmnRenderer extends BpmnRenderer {
     dotElement.setAttribute('style', `fill: red; offset-path: path("${pathDefinition}"); animation: followpath 4s linear infinite;`);
     pathElement.parentElement?.appendChild(dotElement);
     return pathElement;
-
-    // EventBus.js makes it clear that it stops calling renderers when it encou ters a drawConnection function that returns something: https://github.com/bpmn-io/diagram-js/blob/v11.4.1/lib/core/EventBus.js#L373-L377
-    // from https://github.com/bpmn-io/bpmn-js/blob/v11.1.0/lib/draw/BpmnRenderer.js#L1360-L1398
-    const pathData = this.createPathFromConnection(connection);
-
-    const fill = getFillColor(connection, this.defaultFillColor),
-      stroke = getStrokeColor(connection, this.defaultStrokeColor);
-
-    const attrs = {
-      strokeLinejoin: 'round',
-      markerEnd: this.marker('sequenceflow-end', fill, stroke),
-      stroke: getStrokeColor(connection, this.defaultStrokeColor),
-    };
-
-    const path = this.drawPath(visuals, pathData, attrs);
-
-    const sequenceFlow = getSemantic(connection);
-
-    let source;
-
-    if (connection.source) {
-      source = connection.source.businessObject;
-
-      // conditional flow marker
-      if (sequenceFlow.conditionExpression && source.$instanceOf('bpmn:Activity')) {
-        svgAttr(path, {
-          markerStart: this.marker('conditional-flow-marker', fill, stroke),
-        });
-      }
-
-      // default marker
-      if (
-        source.default &&
-        (source.$instanceOf('bpmn:Gateway') || source.$instanceOf('bpmn:Activity')) &&
-        source.default === sequenceFlow
-      ) {
-        svgAttr(path, {
-          markerStart: this.marker('conditional-default-flow-marker', fill, stroke),
-        });
-      }
-    }
-
-    return path;
   }
 }
 
